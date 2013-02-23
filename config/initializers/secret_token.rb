e31e0b671beb2fa525f7d6d29d41fc8c1e862291
Katsunori Kawaguchi <hamakata@gmail.com>
2013-02-24 05:59:44 +0900

    rails new ncomtter -T --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ncomtter::Application.config.secret_token = '46494432df35a8f6c49abbaa26a920204882c5ae7bef2371ac467b3afc0606a6979728f3db7457e9266716b05329ba75bd373cb15714edbab833db032b66b46e'
