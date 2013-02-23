2956922c7a0a111b2cb921f049f911085de18354
Katsunori Kawaguchi <hamakata@gmail.com>
2013-02-24 06:35:33 +0900

    Create twitter authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
   attr_accessible :nickname, :provider, :secret, :token, :uid
+
+  def self.from_omniauth(auth)
+    find_by_provider_and_uid(auth["provider"], auth["uid"]) || create_with_omniauth(auth)
+  end
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.nickname = auth["info"]["nickname"]
+      user.token = auth["credentials"]["token"]
+      user.secret = auth["credentials"]["secret"]
+    end
+  end
 end
