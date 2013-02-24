d83cd5043e75e4e1961ee882aff6b9bca91283ba
Katsunori Kawaguchi <hamakata@gmail.com>
2013-02-24 10:37:59 +0900

    XXX: include twitter consumer token and secret for hackathon

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+ENV["TWITTER_KEY"] = "M2iWm7o7ujngcA5YIfxcEA"
+ENV["TWITTER_SECRET"] = "YZjxnB9EY0R3X49CuJKn8chZkgCgkusRzVkmE2ktHg"
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
