58f0b15e9276510622c6eca1458b81bebebf45a9
Katsunori Kawaguchi <hamakata@gmail.com>
2013-02-24 14:16:12 +0900

    Update user attributes when login via twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,15 @@ class User < ActiveRecord::Base
   attr_accessible :nickname, :provider, :secret, :token, :uid
 
   def self.from_omniauth(auth)
-    find_by_provider_and_uid(auth["provider"], auth["uid"]) || create_with_omniauth(auth)
+    if (user = find_by_provider_and_uid(auth["provider"], auth["uid"]))
+      user.nickname = auth["info"]["nickname"]
+      user.token = auth["credentials"]["token"]
+      user.secret = auth["credentials"]["secret"]
+      user.save! if user.changed?
+    else
+      elsecreate_with_omniauth(auth)
+    end
+    user
   end
 
   def self.create_with_omniauth(auth)
